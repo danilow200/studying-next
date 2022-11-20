0d40f5b9c89c93b6533ad4908d6e2578e8bdff20
Danilo Domingo <40957754+danilow200@users.noreply.github.com>
2022-11-19 21:29:10 -0300

    feat: introducing styled components

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,8 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+
 html,
 body {
   padding: 0;
   margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+  font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
 }
 
@@ -24,3 +28,5 @@ a {
     background: black;
   }
 }
+`
+
---
+++ src/styles/theme.ts
@@ -0,0 +1,26 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  colors: {
+    gray: {
+      1: '#161618',
+      2: '#1C1C1F',
+      3: '#232326',
+      4: '#28282C',
+      5: '#2E2E32',
+      6: '#34343A',
+      7: '#3E3E44',
+      8: '#C8C7CB',
+      9: '#706F78',
+      10: '#7E7D86',
+      11: '#A09FA6',
+      12: '#EDEDEF'
+    },
+  },
+  breakpoints: {
+    mobile: 'only screen and (max-width: 769px)',
+    tablet: 'only screen and (max-width: 1023px)',
+    wide: 'only screen and (max-width: 1350px)',
+    extraWide: 'only screen and (max-width: 1921px)'
+  }
+};
